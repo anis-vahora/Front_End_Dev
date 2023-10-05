c23007eda35db25a23c814e793a82ec470643186
Mahammad Anis Vahora <58881736+anis-vahora@users.noreply.github.com>
2023-10-04 23:23:00 -0400

    Add files via upload

---
+++ lab1/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Greeter from "./Greater";
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
